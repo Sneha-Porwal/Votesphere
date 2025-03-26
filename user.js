7496d0b7e94efd4e8ebea67655ab2f3439f967c3
Sneha-Porwal <142002344+Sneha-Porwal@users.noreply.github.com>
2025-03-26 22:44:43 +0530

    Pages added

---
+++ user.js
@@ -0,0 +1,17 @@
+function viewProfile() {
+    window.location.href = "profile.html";
+}
+
+function viewVerification() {
+    alert("Checking Verification Status...");
+    window.location.href = "voterverification.html";
+}
+
+function castVote() {
+    alert("Redirecting to Voting Page...");
+    window.location.href = "vote.html";
+}
+
+function viewResults() {
+    window.location.href = "result.html";
+}
